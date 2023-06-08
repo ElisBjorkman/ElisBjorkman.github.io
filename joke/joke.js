8d6bc13189a1a162fe5754a016aa277aab9144c2
Elis Björkman <elbj0223@storsjogymnasiet.se>
2023-06-08 10:18:13 +0200

    fixat index.html

---
+++ joke/joke.js
@@ -0,0 +1,26 @@
+const jokeButton = document.getElementById("jokeButton");
+jokeButton.addEventListener("click", getJoke)
+
+const p_joke = document. getElementById("pjoke")
+
+function getJoke(e) {
+    fetch(  
+        `https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,explicit&type=single`,
+        {
+            method: "GET",
+            headers: new Headers({
+                Accept: "application/json"
+            })
+        }
+    )
+        .then(res => res.json())
+        .then(response => {
+            
+            //console.log(`${response.joke}`)
+            let joke = response.joke
+            p_joke.innerHTML = joke;
+
+            
+        })
+        .catch(error => console.log(error));
+}
